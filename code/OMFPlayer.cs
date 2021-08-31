ff2b2025744c4dbaf39bb4094f8868647318891d
goat <16760685+goaaats@users.noreply.github.com>
2021-08-31 03:08:42 +0200

    player use

--- code/OMFPlayer.cs
+++ code/OMFPlayer.cs
@@ -31,10 +31,7 @@ namespace OneMoreFloor
 		{
 			base.Simulate( cl );
 
-			//
-			// If you have active children (like a weapon etc) you should call this to
-			// simulate those too.
-			//
+			this.TickPlayerUse();
 			SimulateActiveChild( cl, ActiveChild );
 
 			//
@@ -50,6 +47,50 @@ namespace OneMoreFloor
 				ragdoll.PhysicsGroup.Velocity = EyeRot.Forward * 1000;
 			}
 		}
+
+		#region Use
+
+		public bool IsUseDisabled()
+		{
+			return ActiveChild is IUse use && use.IsUsable( this );
+		}
+
+		protected override Entity FindUsable()
+		{
+			if ( IsUseDisabled() )
+				return null;
+
+			// First try a direct 0 width line
+			var tr = Trace.Ray( EyePos, EyePos + EyeRot.Forward * (85 * Scale) )
+				.HitLayer( CollisionLayer.Debris )
+				.Ignore( this )
+				.Run();
+
+			// Nothing found, try a wider search
+			if ( !IsValidUseEntity( tr.Entity ) )
+			{
+				tr = Trace.Ray( EyePos, EyePos + EyeRot.Forward * (85 * Scale) )
+					.Radius( 2 )
+					.HitLayer( CollisionLayer.Debris )
+					.Ignore( this )
+					.Run();
+			}
+
+			// Still no good? Bail.
+			if ( !IsValidUseEntity( tr.Entity ) ) return null;
+
+			return tr.Entity;
+		}
+
+		protected override void UseFail()
+		{
+			if ( IsUseDisabled() )
+				return;
+
+			base.UseFail();
+		}
+
+		#endregion
 
 		public override void OnKilled()
 		{
